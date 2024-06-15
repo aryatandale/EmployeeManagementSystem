a6a8726e95794b68618291ba066890cc1d496aea
Arya Tandale <93761073+aryatandale@users.noreply.github.com>
2024-06-15 10:36:53 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# EmployeeManagementSystem
+ The EMS will be a centralized digital platform that allows organization to securely store and access employee’s personal and professional information. Independently conceived, developed, and implemented Employee Management System, showcasing my individual skills and expertise.
---
+++ demo/src/main/java/employee.java
@@ -0,0 +1,23 @@
+public class employee {
+    
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String emailID;
+
+    public employee()
+    {
+
+    }
+
+    public employee(String firstName,String lastName,String emailID)
+    {
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailID = emailID;
+
+        
+
+    }
+}
